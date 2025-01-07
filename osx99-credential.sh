2deec7bd7be0244ceb98973fc50ecc2572669131
JaehoonSong12 <manual20151276@gmail.com>
2025-01-06 22:59:39 -0500

    Normal Execution commit: 2025-01-06_22-59-39 updated by Jaehoon

--- osx99-credential.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Resolving git and Xcode configuration..."
-
-
-
-git credential-cache exit
-git config --global user.name "Your Name"
-git config --global user.email "your_email@example.com"
